241e1ecb6102adf3070b585cf2a2f4f429ebe0c9
Xiang Long <mindsculptor@yeah.net>
2025-06-12 14:06:18 +0800

    Add execution status check to success return

--- verl/tools/sandbox_fusion_tools.py
+++ verl/tools/sandbox_fusion_tools.py
@@ -337,14 +337,18 @@ class SandboxFusionTool(BaseTool):
             response_json = response.json()
             status = response_json["status"]
             if status == "Success":
-                ret_str = ""
-                if response_json["run_result"]["return_code"] is not None and response_json["run_result"]["return_code"] != 0:
-                    ret_str += f"return_code: {response_json['run_result']['return_code']}\n"
-                if response_json["run_result"]["stdout"] is not None and len(response_json["run_result"]["stdout"]) > 0:
-                    ret_str += f"stdout: {response_json['run_result']['stdout']}\n"
-                if response_json["run_result"]["stderr"] is not None and len(response_json["run_result"]["stderr"]) > 0:
-                    ret_str += f"stderr: {response_json['run_result']['stderr']}\n"
-                return ret_str
+                execution_status = response_json["run_result"]["status"]
+                if execution_status == "Finished":
+                    ret_str = ""
+                    if response_json["run_result"]["return_code"] is not None and response_json["run_result"]["return_code"] != 0:
+                        ret_str += f"return_code: {response_json['run_result']['return_code']}\n"
+                    if response_json["run_result"]["stdout"] is not None and len(response_json["run_result"]["stdout"]) > 0:
+                        ret_str += f"stdout: {response_json['run_result']['stdout']}\n"
+                    if response_json["run_result"]["stderr"] is not None and len(response_json["run_result"]["stderr"]) > 0:
+                        ret_str += f"stderr: {response_json['run_result']['stderr']}\n"
+                    return ret_str
+                else:
+                    raise ValueError(f"Unknown execution status: {execution_status}\nresponse: {response.text}")
             elif status == "Failed":
                 execution_status = response_json["run_result"]["status"]
                 # Drop last cell if failed, to avoid keep failed in further execution
@@ -355,6 +359,7 @@ class SandboxFusionTool(BaseTool):
                         ret_str += f"stdout: {response_json['run_result']['stdout']}\n"
                     if response_json["run_result"]["stderr"] is not None and len(response_json["run_result"]["stderr"]) > 0:
                         ret_str += f"stderr: {response_json['run_result']['stderr']}\n"
+                    logger.warning(f"Execution time limit exceeded, time: {response_json['run_result']['execution_time']}, timeout: {payload['run_timeout']}, response: {response.text}")
                     return ret_str
                 elif execution_status == "Finished":
                     ret_str = ""
